ed9936c65c9b9db885e602e2e2e546de76a760cb
Jessica Ferreira <samojess@gmail.com>
2022-09-20 16:57:19 -0300

    Finaliza requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,15 +22,28 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado possui 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
-    // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => { 
+    expect(circle(NaN)).toBe(undefined);
   });
-});
+  it('returns an object', () => {
+    expect(typeof circle(3)).toMatch(/object/);
+  });
+  it('returns 3 properties', () => {
+    expect(Object.keys(circle(3)).length).toBe(3);
+  });
+  it('returns undefined when receives no arguments', () => {
+    expect(circle()).toBe(undefined);
+  });
+  it('returns a number close to 12.56 for a radius of 2', () => {
+    expect(circle(2).circumference).toBeCloseTo(12.56);
+  });
+  it('returns a number close to 18.84', () => {
+    expect(circle(3).area).toBeCloseTo(28.26);
+  });
+  it('returns the correct data for a circle with a radius of 3', () => {
+    expect(circle(3).radius).toBe(3);
+    expect(circle(3).area).toBeCloseTo(28.26);
+    expect(circle(3).circumference).toBeCloseTo(18.84);  
+    })
+  });
+
